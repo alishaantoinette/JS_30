815541d10006e9cad8078162035569bf692e6f89
alishaevn <alieva.inc@gmail.com>
2018-08-17 11:46:34 -0700

    changed single quotes back to double quotes

--- JS_drum_kit/main.js
+++ JS_drum_kit/main.js
@@ -2,15 +2,52 @@
 
 const playSound = e => {
   console.log('event', e);
+  /* 
+  the event being passed into this function is "KeyboardEvent"; it's an object with several properties.
+  for example, when pressing the letter "a" the KeyboardEvent is {
+    altKey : false
+    bubbles : true
+    cancelBubble : false
+    cancelable : true
+    charCode : 0
+    code : "KeyA"
+    composed : true
+    ctrlKey : false
+    currentTarget : null
+    defaultPrevented : false
+    detail : 0
+    eventPhase : 0
+    isComposing : false
+    isTrusted : true
+    key : "a"
+    keyCode : 65
+    location : 0
+    metaKey : false
+    path : (4) [body, html, document, Window]
+    repeat : false
+    returnValue : true
+    shiftKey : false
+    sourceCapabilities : InputDeviceCapabilities {firesTouchEvents: false}
+    srcElement : body
+    target : body
+    timeStamp : 2513.3000000059837
+    type : "keydown"
+    view : Window {postMessage : ƒ, blur: ƒ, focus: ƒ, close: ƒ, frames: Window, …}
+    which : 65
+    __proto__ : KeyboardEvent
+  }
+  */
 
-  const audio = document.querySelector(`audio[data-key='${e.keycode}']`);
-  // find the first element in the page that has a data-key attribute that equals the keycode of the event(keydown) keycode. 
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // find the first audio element on the page that has the same data-key attribute of the keycode property on the passed in KeyboardEvent object. 
   console.log('audio:', audio);
 
-  const key = document.querySelector(`.key[data-key='${e.keycode}']`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  // find the first div element on the page that has the same data-key attribute of the keycode property on the passed in KeyboardEvent object. 
   console.log('key:', key);
 
   if (!audio) return;
+  // if there is no audio element on the page that corresponds to the pressed key, exit the function without doing anything
 
   audio.currentTime = 0;
   audio.play();
