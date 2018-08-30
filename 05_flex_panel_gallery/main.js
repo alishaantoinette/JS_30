be6c54b0272cccfb58c391a02d4daeb966c37fd7
alishaevn <alieva.inc@gmail.com>
2018-08-30 13:39:54 -0700

    major debugging and commenting

--- 05_flex_panel_gallery/main.js
+++ 05_flex_panel_gallery/main.js
@@ -3,16 +3,35 @@
 const panels = document.querySelectorAll('.panel'); // create a node list of all the elements with a class of "panels"
 // console.log('panels:', panels);
 
+
+// A - this doesn't work.
 // const toggleOpen = e => { // pass in the event that triggered this function to be called
-//   console.log('TO target:', e.target);
-//   console.log('TO classlist:', e.target.classList);
-//   // e.target.classList.toggle('open'); // add or remove the "open" class to/from the element that triggered this function
+//   // console.log('event:', e); // the MouseEvent object
+//   console.log('target:', e.target); // if the 2nd "p" tag within the div is clicked, the target always refers to that tag. the first time you click the first OR thrid "p" tags within the div, the target is the div. if you try clicking any "p" tag after that, the target is always that tag. therefore, the div never shrinks because the "p" tag is having the "open" class added and removed (which there is no css set up for) instead of the div. 
+//   // console.log('classlist:', e.target.classList); // the classList consists of the 2 classes on the div in the html, plus "open" and "open-active"
+//   e.target.classList.toggle('open'); // add or remove the "open" class to/from the element that triggered this function
 // };
 
+
+//  B - this doesn't work. 
+// const toggleOpen = () => {
+//   console.log('this:', this); // the Window object
+//   this.classList.toggle('open'); // Uncaught TypeError: Cannot read property 'toggle' of undefined at HTMLDivElement.toggleOpen
+// };
+
+
+// C - this works
 function toggleOpen() {
-  console.log('this:', this);
+  console.log('this:', this); // the entire .panel div that was clicked
   this.classList.toggle('open');
 }
+
+// D - this doesn't work, it behaves the same way as A
+// function toggleOpen(e) {
+//   console.log('target:', e.target);
+//   e.target.classList.toggle('open');
+// }
+
 
 const toggleActive = e => { // this runs twice for every panel div that's clicked because the top text and bottom text transition
   console.log('name:', e.propertyName);
@@ -23,3 +42,7 @@ const toggleActive = e => { // this runs twice for every panel div that's clicke
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen)); // add an event listener to each element in the node list that will invoke the "toggleOpen" function when clicked
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+
+// Resource: https://dmitripavlutin.com/when-not-to-use-arrow-functions-in-javascript/
