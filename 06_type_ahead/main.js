55607e06e518caae731eae81169e89366c0b9582
alishaevn <alieva.inc@gmail.com>
2018-09-08 17:45:17 -0700

    comments

--- 06_type_ahead/main.js
+++ 06_type_ahead/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// =============== GET DATA FROM THE ENDPOINT ===============
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cityData = [];
@@ -14,6 +15,7 @@ fetch(endpoint)
   .then(data => cityData.push(...data));
 
 
+// =============== HANDLE THE DATA ===============
 const findMatchingCitiesAndStates = (charactersThatWereTyped, cityData) =>
   cityData.filter(cityStat => {
     const regexp = new RegExp(charactersThatWereTyped, 'gi');
@@ -31,7 +33,9 @@ const showMatchingCitiesAndStates = () => {
   matchingCitiesAndStates.innerHTML = matchesListItems;
 };
 
-const charactersBeingTyped = document.querySelector('.search');
-const matchingCitiesAndStates = document.querySelector('.suggestions');
 
-charactersBeingTyped.addEventListener('keyup', showMatchingCitiesAndStates);
+// =============== DOCUMENT SELECTORS ===============
+const charactersBeingTyped = document.querySelector('.search'); 
+const matchingCitiesAndStates = document.querySelector('.suggestions'); 
+
+charactersBeingTyped.addEventListener('keyup', showMatchingCitiesAndStates); 
