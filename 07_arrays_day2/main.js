9c81c3149f449cc0adeec7b33638a8ce58c8725b
alishaevn <alieva.inc@gmail.com>
2018-09-17 23:31:09 -0700

    deleteId

--- 07_arrays_day2/main.js
+++ 07_arrays_day2/main.js
@@ -16,12 +16,12 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
-const oneNineteenOrOlder = people.some(person => ((new Date().getFullYear() - person.year) >= 19));
-console.log({ oneNineteenOrOlder });
+const oneIsNineteenOrOlder = people.some(person => ((new Date().getFullYear() - person.year) >= 19));
+console.log({ oneIsNineteenOrOlder });
 
 // Array.prototype.every() // is everyone 19 or older?
-const allNineteenOrOlder = people.every(person => ((new Date().getFullYear() - person.year) >= 19));
-console.log({ allNineteenOrOlder });
+const allAreNineteenOrOlder = people.every(person => ((new Date().getFullYear() - person.year) >= 19));
+console.log({ allAreNineteenOrOlder });
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
@@ -32,3 +32,7 @@ console.log({ findId });
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const findIndex = comments.findIndex(comment => comment.id === 823423);
+const deleteId = comments.splice(findIndex, 1);
+console.log({ deleteId });
+console.log({ comments });
