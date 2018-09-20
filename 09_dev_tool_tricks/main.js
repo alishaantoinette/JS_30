33c9d8d341834eef615fa99793012af7283989ce
alishaevn <alieva.inc@gmail.com>
2018-09-19 21:44:05 -0700

    makeGreen()

--- 09_dev_tool_tricks/main.js
+++ 09_dev_tool_tricks/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const makeGreen = () => {
+  const para = document.querySelector('p');
+  para.style.color = '#deaf17';
+  para.style.fontSize = '50px';
+  // console.log('para:', para);
+};
